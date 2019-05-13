e35a368159998f6277e788d0cd36ef0f546bf84e
JianmingZhang <zhang.jianming@opg.cn>
2019-05-13 22:26:17 +0800

    commit PlusOne

--- club.isparkle.leetcode/array/PlusOne.java
+++ club.isparkle.leetcode/array/PlusOne.java
@@ -46,4 +46,20 @@ public class PlusOne {
         return digits;
     }
 
+    public int[] plusOneMoreGood(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            if(i > 0) {
+                digits[i] = 0;
+            }else {
+                int[] arr = new int[digits.length + 1];
+                arr[0] = 1;
+                return arr;
+            }
+        }
+        return null;
+    }
 }
