560ea96ce50750e1558223d215f82d988c2514d3
JianmingZhang <zhang.jianming@opg.cn>
2019-05-22 20:27:45 +0800

    commit ToeplitzMatrix

---
+++ club.isparkle.leetcode/array/ToeplitzMatrix.java
@@ -0,0 +1,52 @@
+package array;
+
+/**
+ * 766. Toeplitz Matrix
+ * <p>
+ * A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+ * Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+ * <p>
+ * Example 1:
+ * Input:
+ * matrix = [
+ * [1,2,3,4],
+ * [5,1,2,3],
+ * [9,5,1,2]
+ * ]
+ * <p>
+ * Output: True
+ * Explanation:
+ * In the above grid, the diagonals are:
+ * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+ * In each diagonal all elements are the same, so the answer is True.
+ * <p>
+ * Example 2:
+ * Input:
+ * matrix = [
+ * [1,2],
+ * [2,2]
+ * ]
+ * Output: False
+ * Explanation:
+ * The diagonal "[1, 2]" has different elements.
+ * Note:
+ * matrix will be a 2D array of integers.
+ * matrix will have a number of rows and columns in range [1, 20].
+ * matrix[i][j] will be integers in range [0, 99].
+ * <p>
+ * Created by zjm on 2019/5/22.
+ */
+public class ToeplitzMatrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        if (matrix.length < 2 || matrix[0].length < 2) {
+            return true;
+        }
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[0].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) return false;
+            }
+        }
+        return true;
+    }
+}
