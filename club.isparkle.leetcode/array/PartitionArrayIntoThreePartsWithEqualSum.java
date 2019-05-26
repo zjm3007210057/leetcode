824b10f221477d05b7175dd18defe30d3e76c3ca
JianmingZhang <zhang.jianming@opg.cn>
2019-05-26 21:46:43 +0800

    commit PartitionArrayIntoThreePartsWithEqualSum

---
+++ club.isparkle.leetcode/array/PartitionArrayIntoThreePartsWithEqualSum.java
@@ -0,0 +1,53 @@
+package array;
+
+/**
+ * 1013. Partition Array Into Three Parts With Equal Sum
+ *
+ * Given an array A of integers, return true if and only if we can partition the array into three non-empty parts with equal sums.
+ * Formally, we can partition the array if we can find indexes i+1 < j with (A[0] + A[1] + ... + A[i] ==
+ * A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
+ *
+ * Example 1:
+ * Input: [0,2,1,-6,6,-7,9,1,2,0,1]
+ * Output: true
+ * Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+ *
+ * Example 2:
+ * Input: [0,2,1,-6,6,7,9,-1,2,0,1]
+ * Output: false
+ *
+ * Example 3:
+ * Input: [3,3,6,5,-2,2,5,1,-9,4]
+ * Output: true
+ * Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+ *
+ * Note:
+ * 3 <= A.length <= 50000
+ * -10000 <= A[i] <= 10000
+ *
+ * Created by zjm on 2019/5/26.
+ */
+public class PartitionArrayIntoThreePartsWithEqualSum {
+
+    //first, calculate the sum of A, if sum can not divided by 3, return false.
+    //then, just find two index in A which makes A[0] + A[1] + ... + A[index1] = sum / 3 and A[index1+1] + ... + A[index2] = sum / 3
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum = 0;
+        for(int i = 0; i < A.length; i++) {
+            sum += A[i];
+        }
+        if(sum % 3 != 0) return false;
+        sum /= 3;
+        int tmp = 0;
+        int count = 0;
+        for(int i = 0; i < A.length; i++) {
+            tmp += A[i];
+            if(tmp == sum) {
+                count++;
+                tmp = 0;
+            }
+            if(count == 2) return true;
+        }
+        return false;
+    }
+}
