faf58fde2271d8181ae56d8761389cfc653d3b6c
zjm3007210057 <zjm3007210057@163.com>
2019-05-27 22:08:45 +0800

    commit FourSum

---
+++ club.isparkle.leetcode/array/FourSum.java
@@ -0,0 +1,71 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 18. 4Sum
+ *
+ * Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that
+ * a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+ *
+ * Note:
+ * The solution set must not contain duplicate quadruplets.
+ *
+ * Example:
+ * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+ * A solution set is:
+ * [
+ *  [-1,  0, 0, 1],
+ *  [-2, -1, 1, 2],
+ *  [-2,  0, 0, 2]
+ * ]
+ *
+ * Created by zjm on 2019/5/27.
+ */
+public class FourSum {
+
+    //solution just like ThreeSum
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList();
+        int  tmp;
+        for(int i = 0; i < nums.length - 3; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            for(int j = i + 1; j < nums.length - 2; j++) {
+                if(j > i + 1 && nums[j] == nums[j-1]) {
+                    continue;
+                }
+                int l = j + 1;
+                int r = nums.length - 1;
+                tmp = target - nums[i] - nums[j];
+                while(l < r) {
+                    if(nums[l] + nums[r] == tmp) {
+                        List<Integer> list = new ArrayList(4);
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[l]);
+                        list.add(nums[r]);
+                        res.add(list);
+                        while(l < r && nums[l] == nums[l+1]) {
+                            l++;
+                        }
+                        while(l < r && nums[r] == nums[r-1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    }else if(nums[l] + nums[r] > tmp) {
+                        r--;
+                    }else {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
--- club.isparkle.leetcode/array/ThreeSumClosest.java
+++ club.isparkle.leetcode/array/ThreeSumClosest.java
@@ -4,39 +4,39 @@ import java.util.Arrays;
 
 /**
  * 16. 3Sum Closest
- *
+ * <p>
  * Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.
  * Return the sum of the three integers. You may assume that each input would have exactly one solution.
- *
+ * <p>
  * Example:
  * Given array nums = [-1, 2, 1, -4], and target = 1.
  * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2)
- *
+ * <p>
  * Created by zjm on 2019/5/27.
  */
 public class ThreeSumClosest {
 
     //the solution is just like ThreeSum
     public int threeSumClosest(int[] nums, int target) {
-    Arrays.sort(nums);
-    int res = nums[0] + nums[1] + nums[2];
-    int diff = Math.abs(res - target);
-    int tmp;
-    int tmpDiff;
-    for(int i = 0; i < nums.length; i++) {
-        int l = i + 1;
-        int r = nums.length - 1;
-        while(l < r) {
-            tmp = nums[i] + nums[l] + nums[r];
-            tmpDiff = Math.abs(tmp -target);
-            if(tmpDiff < diff) {
-                res = tmp;
-                diff = tmpDiff;
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];
+        int diff = Math.abs(res - target);
+        int tmp;
+        int tmpDiff;
+        for (int i = 0; i < nums.length; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                tmp = nums[i] + nums[l] + nums[r];
+                tmpDiff = Math.abs(tmp - target);
+                if (tmpDiff < diff) {
+                    res = tmp;
+                    diff = tmpDiff;
+                }
+                if (tmp > target) r--;
+                else l++;
             }
-            if(tmp > target) r--;
-            else l++;
         }
+        return res;
     }
-    return res;
-}
 }
