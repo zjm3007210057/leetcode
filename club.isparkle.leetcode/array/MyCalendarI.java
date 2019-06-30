caf06353ba550ccff6bf776091f88eb8c7946815
zjm3007210057 <zjm3007210057@163.com>
2019-06-30 16:46:28 +0800

    commit MyCalendarI

---
+++ club.isparkle.leetcode/array/MyCalendarI.java
@@ -0,0 +1,58 @@
+package array;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 729. My Calendar I
+ * <p>
+ * Implement a MyCalendar class to store your events. A new event can be added if adding the event will not cause a double booking.
+ * <p>
+ * Your class will have the method, book(int start, int end). Formally, this represents a booking on the half open
+ * interval [start, end), the range of real numbers x such that start <= x < end.
+ * <p>
+ * A double booking happens when two events have some non-empty intersection (ie., there is some time that is common to both events.)
+ * <p>
+ * For each call to the method MyCalendar.book, return true if the event can be added to the calendar successfully
+ * without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+ * <p>
+ * Your class will be called like this: MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end)
+ * Example 1:
+ * <p>
+ * MyCalendar();
+ * MyCalendar.book(10, 20); // returns true
+ * MyCalendar.book(15, 25); // returns false
+ * MyCalendar.book(20, 30); // returns true
+ * Explanation:
+ * The first event can be booked.  The second can't because time 15 is already booked by another event.
+ * The third event can be booked, as the first event takes every time less than 20, but not including 20.
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * The number of calls to MyCalendar.book per test case will be at most 1000.
+ * In calls to MyCalendar.book(start, end), start and end are integers in the range [0, 10^9].
+ * <p>
+ * Created by zjm on 2019/6/29 19:40
+ */
+public class MyCalendarI {
+
+    private TreeMap<Integer, Integer> map;
+
+    public MyCalendarI() {
+        map = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        Integer smallKey = map.floorKey(end-1);
+        Integer largeKey = map.ceilingKey(start);
+        if(smallKey != null && map.get(smallKey) > start) {
+            return false;
+        }
+        if(largeKey != null && largeKey < end) {
+            return false;
+        }
+        map.put(start, end);
+        return true;
+    }
+}
