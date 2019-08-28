fb75913fe1890c2f661aa76092a76d3883870714
zjm3007210057 <zjm3007210057@163.com>
2019-08-28 20:12:29 +0800

    add DefangingAnIPAddress

---
+++ club.isparkle.leetcode/string/DefangingAnIPAddress.java
@@ -0,0 +1,36 @@
+package string;
+
+/**
+ * 1108. Defanging an IP Address
+ * <p>
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+ * A defanged IP address replaces every period "." with "[.]".
+ * <p>
+ * Example 1:
+ * Input: address = "1.1.1.1"
+ * Output: "1[.]1[.]1[.]1"
+ *
+ * Example 2:
+ * Input: address = "255.100.50.0"
+ * Output: "255[.]100[.]50[.]0"
+ * <p>
+ * Constraints:
+ * The given address is a valid IPv4 address
+ * <p>
+ * Created by zjm on 2019/8/28 20:07
+ */
+public class DefangingAnIPAddress {
+
+    //一次循环字符数组，直接将点号.换成[.]
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : address.toCharArray()) {
+            if(c != '.') {
+                sb.append(c);
+            }else {
+                sb.append("[.]");
+            }
+        }
+        return sb.toString();
+    }
+}
